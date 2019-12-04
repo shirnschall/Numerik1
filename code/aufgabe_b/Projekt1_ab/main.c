43b0664d7c0899e3775c3b2983d2d683e2457930
RafaelDorigo <e11819554@student.tuwien.ac.at>
2019-12-04 23:51:44 +0100

    latex beweis a, code aufgabe b

--- code/aufgabe_b/Projekt1_ab/main.c
+++ code/aufgabe_b/Projekt1_ab/main.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double g(double);
 double f(double);
+double sumtrapez(int,int);
 
-int main(){
-
-    int i;
-    printf("x\ty\tfunktion\n");
-    for(i=0; i<=255 ; i++) {
-        printf("%f\t%f\t%s\n",(double) i / 100,g((double) i / 100),"sin(x)/x");
-        printf("%f\t%f\t%s\n",(double) i / 100,f((double) i / 100),"x");
+int main()
+{
+    double t = 2;
+    int i, j, n;
+    printf("t\tn\ty\tfunktion\n");
+    for(j = 1; j <= 10; j++)
+    {
+        for(i=1; i<=20 ; i++)
+        {
+            printf("%d\t%d\t%f\t%s\n", j, i,sumtrapez(j,i),"x*x");
+            //printf("Fehler ist:");
+            //printf("");
+        }
     }
+
 
     return 0;
 }
 
-double g(double x){
-    //sin(x)/x      sonst
-    //1             x=0
-    return abs(x)>10e-14?sin(x)/x : (double)1;
+double f(double x)
+{
+    return x*x; //sin(x)/x*exp(-x);
 }
 
-double f(double x){
-    return x;
-
+double sumtrapez(int t, int n){
+    double x, h, s = 0;
+    int j;
+    h = (double) t / n;
+    for(j = 1; j <= n - 1; j++)
+    {
+        x = j * h;
+        s += f(x);
+    }
+    return (double) (h / 2) * (f(0) + 2 * s + f(t));
 }
