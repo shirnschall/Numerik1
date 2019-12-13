684481aa01f05ab96c5f9e52a4e9f677b16c9b4e
RafaelDorigo <e11819554@student.tuwien.ac.at>
2019-12-13 23:12:14 +0100

    programm h und latex plots

---
+++ latex/fehlerTsinx.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def f(x):
+    return np.sin(x)/x*np.exp(-x)
+
+def sumtrapez(t,h):
+    xs=np.arange(0,t,h)
+    fs=f(xs)
+    return h/2*(1+2*sum(fs[1:-1])+fs[-1]) # 1=f(0) stetig fortgesetzt
+
+T=10
+errors =[]
+hs = 1/2**np.arange(15)
+for h in hs:
+    errors.append(abs(np.pi/4-sumtrapez(T,h)))
+plt.loglog(hs,errors)
+plt.grid(which='major', linewidth=0.5)
+plt.xlabel('h', size='large')
+plt.ylabel('Fehler', size='large')
+plt.show()
+plt.savefig('FehlerplotTfix.png', dpi=400)
---
+++ latex/test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+def f(x):
+    return np.exp(-x)
+
+def sumtrapez(t,h):
+    xs=np.arange(0,t,h)
+    fs=f(xs)
+    return h/2*(1+2*sum(fs[1:-1])+fs[-1]) # 1=f(0) stetig fortgesetzt
+
+h=0.001
+errors =[]
+T = 2**np.arange(12)
+for t in T:
+    errors.append(abs(1-sumtrapez(t,h)))
+plt.loglog(T,errors)
+plt.grid(which='major', linewidth=0.5)
+plt.xlabel('T', size='large')
+plt.ylabel('Fehler', size='large')
+plt.show()
+#plt.savefig('Fehlerploth.png', dpi=400)
