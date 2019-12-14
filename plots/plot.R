6f3ac6b202b8ef5858ea71184431ad70884fc21b
Sebastian Hirnschall <sebastian@hirnschall.net>
2019-12-14 23:23:37 +0100

    aufgabe-e daten von aufgabe a,b

--- plots/plot.R
+++ plots/plot.R
@@ -4,7 +4,8 @@ require(dplyr)
 
 data=read.csv("/Users/shirnschall/Desktop/Numerik1/code/aufgabe-d/aufgabe-e",header = TRUE ,sep = "\t")
 
-ggplot(data,aes(x=Quadraturpunkte,y=Fehler,color=Funktion,group=Funktion))+
-  geom_point() + geom_path(aes(group = Funktion)) 
+p <- ggplot(data,aes(x=Quadraturpunkte,y=Fehler,color=Funktion,group=Funktion))+
+  geom_point() + geom_path(aes(group = Funktion))
+p
 
 ggsave("aufgabe-e.png", units="in", width=5, height=4, dpi=300)
